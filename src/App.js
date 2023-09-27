ea3f58ed1abcf300f7d9f008d5b79995e43adc61
Minh Duy <duyxitrum000@gmail.com>
2023-09-27 11:55:18 +0700

    Setup scss bootstrap router-dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+
 import './App.scss';
+import Login from './Components/Login/Login';
+import Nav from './Components/Navigation/Nav';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function NotFound() {
+  return <h1>404 - Page Not Found</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world React with Hoi Dan IT
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='app-container'>
+        <Nav></Nav>
+        <Routes>
+          <Route path="/" exact  >home</Route>
+          <Route path="/news" >News</Route>
+          <Route path="/about" >About</Route>
+          <Route path="/contact" >Contact</Route>
+
+          <Route path='/login' element={<Login />} />
+
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </div>
+    </Router>
+
   );
 }
 
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,12 @@
+import './login.scss'
+
+const Login = () => {
+    return (
+        <div className='login-container'>
+            <h1>Login</h1>
+            <button className='btn btn-primary'>Add</button>
+        </div>
+    )
+}
+
+export default Login;
