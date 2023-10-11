d0eeff2a0b9c948a39546f8461cb8499a064f99f
Minh Duy <duyxitrum000@gmail.com>
2023-10-11 09:28:12 +0700

    Handle error toast axios

--- src/setup/axios.js
+++ src/setup/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-
+import { toast } from 'react-toastify';
 const instance = axios.create({
     baseURL: 'http://localhost:8888'
 });
@@ -21,10 +21,50 @@ instance.interceptors.response.use(function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     return response.data;
-}, function (error) {
+}, function (err) {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
-    return Promise.reject(error);
+    const status = err && err.response && err.response.status || 500;
+
+    switch (status) {
+        // authentication (token related issues)
+        case 401: {
+            toast.error("Authenticated User. Please Login...")
+            return Promise.reject(err);
+        }
+
+        // forbidden (permission related issues)
+        case 403: {
+            toast.error("User don't have access permission to this resource....")
+
+            return Promise.reject(err);
+        }
+
+        // bad request
+        case 400: {
+            return Promise.reject(err);
+        }
+
+        // not found
+        case 404: {
+            return Promise.reject(err);
+        }
+
+        // conflict
+        case 409: {
+            return Promise.reject(err);
+        }
+
+        // unprocessable
+        case 422: {
+            return Promise.reject(err);
+        }
+
+        // generic api error (server related) unexpected
+        default: {
+            return Promise.reject(err);
+        }
+    }
 });
 
 
